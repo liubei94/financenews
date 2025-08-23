12f739adadb2ca57d5860cf615f28ab0447ff5f6
liubei94 <100245096+liubei94@users.noreply.github.com>
2025-08-23 21:04:11 +0900

    Update news_workflow.py

--- news_workflow.py
+++ news_workflow.py
@@ -18,7 +18,8 @@ import json
 
 # --- [NEW] crawl4ai and Pydantic imports ---
 from pydantic import BaseModel, Field
-from crawl4ai import AsyncWebCrawler, CrawlerConfig, LLMConfig
+from crawl4ai.web_crawler import AsyncWebCrawler
+from crawl4ai.config import CrawlerConfig, LLMConfig
 from crawl4ai.extraction_strategy import LLMExtractionStrategy
 # ---------------------------------------------
 
@@ -520,4 +521,5 @@ def extract_pubdate_from_item(item):
         except:
             return None
     return None
+
 
